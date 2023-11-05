26b3a695534ed2612426e469bee489c453e392d1
Pratik Rajput <pratikrajput1199@gmail.com>
2023-11-05 16:02:07 +0530

    Push latest changes

--- src/App.js
+++ src/App.js
@@ -18,28 +18,34 @@ const App = () => {
   const api_key = process.env.REACT_APP_API_KEY;
 
   return (
-    <BrowserRouter>
-      <Header/>
-      <div className="App">
-        <Container sx={{width:'100%',backgroundColor:'#00000059'}}>
-          <Routes>
-            <Route exact path="/Trending"  element={<Trending/>} />
-            <Route path="/Trending/:media_type/:id" element={<TrailerSection/>}/>
-            <Route path="/Movies" element={<Movies/>} />
-            <Route path="/Movies/:id" element={<TrailerSectionMovies/>}/>
-            <Route path="/tvseries" element={<Tvseries/>}/>
-            <Route path="/tvseries/:id" element={<TrailerSectionTV/>}/>
-            <Route path="/search" element={<Search/>}/>
-            <Route path="/search/:media_type/:id" element={<TrailerSection/>}/>
-          </Routes>
-        </Container>
-      </div>
-      
-      <div className="simplebottomnavigation">
-      <SimpleBottomNavigation/>
-      </div>
-      
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <div className="App">
+          <Container sx={{ width: "100%", backgroundColor: "#00000059" }}>
+            <Routes>
+              <Route exact path="/Trending" element={<Trending />} />
+              <Route
+                path="/Trending/:media_type/:id"
+                element={<TrailerSection />}
+              />
+              <Route path="/Movies" element={<Movies />} />
+              <Route path="/Movies/:id" element={<TrailerSectionMovies />} />
+              <Route path="/tvseries" element={<Tvseries />} />
+              <Route path="/tvseries/:id" element={<TrailerSectionTV />} />
+              <Route path="/search" element={<Search />} />
+              <Route
+                path="/search/:media_type/:id"
+                element={<TrailerSection />}
+              />
+            </Routes>
+          </Container>
+        </div>
+        <div className="simplebottomnavigation">
+          <SimpleBottomNavigation />
+        </div>
+      </BrowserRouter>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -9,7 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <App />
-
 );
 
 // If you want to start measuring performance in your app, pass a function
